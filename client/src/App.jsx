6deb1951ac0734d3073ccb858a70f06ef9800a6c
Atharva Datta Ghogare <atharva007ag@gmail.com>
2025-08-03 01:35:38 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -21,7 +21,7 @@ function App() {
   return (
     <div className="p-0 m-0 w-full min-h-screen flex flex-col">
       <nav
-        className={`${showMenu ? 'w-[85vw] md:w-1/3' : 'w-0'} h-14 fixed top-4 right-4 rounded-3xl text-base flex flex-wrap justify-center gap-2 items-center bg-zinc-800 overflow-hidden transition-all duration-700 z-[100] px-2`}>
+        className={`${showMenu ? 'w-[85vw] md:w-1/3' : 'w-0'} h-14 fixed top-4 right-1/3 rounded-3xl text-base flex flex-wrap justify-center gap-2 items-center bg-zinc-800 overflow-hidden transition-all duration-700 z-[100] px-2`}>
         <a href="#home" className="text-white px-3 py-2 hover:border-b-4 hover:border-blue-500 transition-all duration-300">Home</a>
         <a href="#about" className="text-white px-3 py-2 hover:border-b-4 hover:border-purple-500 transition-all duration-300">About</a>
         <a href="#skill" className="text-white px-3 py-2 hover:border-b-4 hover:border-green-500 transition-all duration-300">Skill</a>
